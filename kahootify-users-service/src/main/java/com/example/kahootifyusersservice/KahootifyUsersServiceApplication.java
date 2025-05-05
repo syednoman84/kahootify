a2017c0f4669ff131bb5b951a3f151826678ab6d
Noman Ahmed <noman.ahmed@amount.com>
2025-05-05 14:59:14 -0400

    First commit

---
+++ kahootify-users-service/src/main/java/com/example/kahootifyusersservice/KahootifyUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kahootifyusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahootifyUsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KahootifyUsersServiceApplication.class, args);
+    }
+
+}
